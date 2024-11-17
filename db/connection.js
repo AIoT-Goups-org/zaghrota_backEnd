657c642f53f62faed90d4d2198cfdc2fed1ee265
Elksass315 <qsasomar@gmail.com>
2024-11-17 09:17:58 +0200

    init

--- db/connection.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-export const connectDB = async ()=>{
-  await mongoose.connect("mongodb://localhost:27017/weddingapp").then(()=>console.log('Db Connected')).catch(()=>console.log('something went wrong'))
-}
-

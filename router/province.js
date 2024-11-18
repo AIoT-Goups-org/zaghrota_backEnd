01e212c8172ec9040d22b471d9857d5987f66747
Elksass315 <qsasomar@gmail.com>
2024-11-18 11:03:32 +0200

    Sorting Provinces

--- router/province.js
+++ router/province.js
@@ -5,7 +5,30 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
     const provinces = await Province.find();
-    res.send(provinces);
+
+    const importantProvincesOrder = [
+        "القاهره",
+        "الجيزه",
+        "الاسكندريه",
+        "الاسماعليه",
+        "الدقهليه",
+        "الغربيه",
+        "الشرقيه",
+        "المنوفيه",
+        "البحيره",
+        "بور سعيد",
+        "دمياط",
+        "كفر الشيخ",
+    ];
+
+    const sortedProvinces = provinces.sort((a, b) => {
+        const indexA = importantProvincesOrder.indexOf(a.name);
+        const indexB = importantProvincesOrder.indexOf(b.name);
+        return (indexA === -1 ? Number.MAX_VALUE : indexA) - (indexB === -1 ? Number.MAX_VALUE : indexB);
+    });
+
+    res.send(sortedProvinces);
 });
+
 
 export default router;
